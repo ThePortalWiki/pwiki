21d06c19b1b08f159b234f6e96255831b5973421
Etienne Perot <etienne@perot.me>
2017-01-22 15:35:51 -0800

    Add script to mount/unmount /w/images with fusecompress.

---
+++ scripts/fusecompress-mount.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+WEBROOT="$HOME/www"
+IMAGES_ROOT="$HOME/www-private/pwiki-images"
+FUSECOMPRESS="$HOME/fusecompress/install/bin/fusecompress"
+
+if [ ! -d "$WEBROOT" ]; then
+	echo "Cannot find MediaWiki root '$WEBROOT'." >&2
+	exit 1
+fi
+
+if [ ! -d "$IMAGES_ROOT" ]; then
+	echo "Cannot find images root '$IMAGES_ROOT'." >&2
+	exit 1
+fi
+
+if [ ! -x "$FUSECOMPRESS" ]; then
+	echo "Cannot find fusecompress binary '$FUSECOMPRESS'." >&2
+	exit 1
+fi
+
+if [ "$#" != 1 ]; then
+	echo "Usage: $0 <mount|unmount>" >&2
+	exit 1
+fi
+
+MOUNTPOINT="$WEBROOT/w/images"
+
+if [ "$1" == mount ]; then
+	if [ ! -d "$MOUNTPOINT" ]; then
+		mkdir --mode=700 "$MOUNTPOINT"
+	fi
+	if [ "$(ls -1 "$MOUNTPOINT" | wc -l)" -ne 0 ]; then
+		echo "Mountpoint '$MOUNTPOINT' is not empty." >&2
+		exit 1
+	fi
+	"$FUSECOMPRESS" -o allow_other,umask=0002 -c lzma -l 9 "$IMAGES_ROOT" "$MOUNTPOINT"
+	exit 0
+fi
+
+if [ "$1" == unmount ]; then
+	if [ "$(ls -1 "$MOUNTPOINT" | wc -l)" -eq 0 ]; then
+		echo "Mountpoint '$MOUNTPOINT' is empty." >&2
+		exit 1
+	fi
+	fusermount -u -z "$MOUNTPOINT"
+	rmdir "$MOUNTPOINT"
+	exit 0
+fi
+
+echo 'First argument must be "mount" or "unmount".' >&2
+exit 1
