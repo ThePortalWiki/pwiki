163c9a69a58514b290ae5536add74baa0a8d6662
Etienne Perot <etienne@perot.me>
2025-05-18 15:46:18 -0700

    Upgrade to MediaWiki 1.43.

--- scripts/setup-gvisor.sh
+++ scripts/setup-gvisor.sh
@@ -2,48 +2,59 @@
 
 set -euo pipefail
 
-RUNSC_PATH=/usr/bin/runsc
 BASE_URL="https://storage.googleapis.com/gvisor/releases/release/latest/$(uname -m)"
-
-need_redownload=false
-if [[ ! -f "$RUNSC_PATH" ]]; then
-	need_redownload=true
-else
-	latest_sha512="$(curl -fSsl "$BASE_URL/runsc.sha512" | cut -d' ' -f1)"
-	if [[ -z "$latest_sha512" ]]; then
-		echo 'Cannot get latest release hash.' >&2
-		exit 1
-	fi
-	if [[ "$(sha512sum "$RUNSC_PATH" | cut -d' ' -f1)" != "$latest_sha512" ]]; then
+INSTALL_PATH=/usr/bin
+BINARIES=(
+	runsc
+	containerd-shim-runsc-v1
+)
+redownloaded=false
+for binary in "${BINARIES[@]}"; do
+	BINARY_PATH="/usr/bin/$binary"
+	need_redownload=false
+	if [[ ! -f "$BINARY_PATH" ]]; then
 		need_redownload=true
+	else
+		latest_sha512="$(curl -fSsl "$BASE_URL/$binary.sha512" | cut -d' ' -f1)"
+		if [[ -z "$latest_sha512" ]]; then
+			echo 'Cannot get latest release hash.' >&2
+			exit 1
+		fi
+		if [[ "$(sha512sum "$BINARY_PATH" | cut -d' ' -f1)" != "$latest_sha512" ]]; then
+			need_redownload=true
+		fi
 	fi
-fi
 
-if [[ "$need_redownload" == false ]]; then
-	exit 0
-fi
-
-while true; do
-	rm -f "$RUNSC_PATH.tmp" &>/dev/null || true
-	if ! curl -fSsl "$BASE_URL/runsc" > "$RUNSC_PATH.tmp"; then
-		echo 'Cannot get latest release.' >&2
-		exit 1
-	fi
-	latest_sha512="$(curl -fSsl "$BASE_URL/runsc.sha512" | cut -d' ' -f1)"
-	if [[ -z "$latest_sha512" ]]; then
-		echo 'Cannot get latest release hash.' >&2
-		exit 1
-	fi
-	chown root:root "$RUNSC_PATH.tmp"
-	chmod 755 "$RUNSC_PATH.tmp"
-	if [[ "$(sha512sum "$RUNSC_PATH.tmp" | cut -d' ' -f1)" != "$latest_sha512" ]]; then
-		echo 'Hash mismatch, retrying...' >&2
-		rm -f "$RUNSC_PATH.tmp" &>/dev/null || true
+	if [[ "$need_redownload" == false ]]; then
 		continue
 	fi
-	mv "$RUNSC_PATH.tmp" "$RUNSC_PATH"
-	break
+
+	while true; do
+		rm -f "$BINARY_PATH.tmp" &>/dev/null || true
+		if ! curl -fSsl "$BASE_URL/$binary" > "$BINARY_PATH.tmp"; then
+			echo 'Cannot get latest release.' >&2
+			exit 1
+		fi
+		latest_sha512="$(curl -fSsl "$BASE_URL/$binary.sha512" | cut -d' ' -f1)"
+		if [[ -z "$latest_sha512" ]]; then
+			echo 'Cannot get latest release hash.' >&2
+			exit 1
+		fi
+		chown root:root "$BINARY_PATH.tmp"
+		chmod 755 "$BINARY_PATH.tmp"
+		if [[ "$(sha512sum "$BINARY_PATH.tmp" | cut -d' ' -f1)" != "$latest_sha512" ]]; then
+			echo 'Hash mismatch, retrying...' >&2
+			rm -f "$BINARY_PATH.tmp" &>/dev/null || true
+			continue
+		fi
+		mv "$BINARY_PATH.tmp" "$BINARY_PATH"
+		break
+	done
+	redownloaded=true
 done
 
-"$RUNSC_PATH" install -- --host-uds=all
+if [[ "$redownloaded" == false ]]; then
+	exit 0
+fi
+/usr/bin/runsc install -- --host-uds=all
 systemctl restart docker
